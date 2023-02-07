aeafadb63c249107651ddf695e3714c85a6b7a20
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-02-07 17:25:24 +0530

    separed the components

---
+++ components/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import {FC} from 'react';
+
+interface Props{}
+
+const HomePage:FC<Props> =(props):JSX.Element=>{
+  return(
+    <div>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage;
