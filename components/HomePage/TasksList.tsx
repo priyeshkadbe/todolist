71f1b92f9e502b16d99e7e21bf2cf85b56f2ab8b
priyeshkadbe <priyeshkadbe49@gmail.com>
2023-02-07 21:42:22 +0530

    working on connect wallet button

--- components/HomePage/TasksList.tsx
+++ components/HomePage/TasksList.tsx
@@ -1,13 +1,20 @@
-
-function TasksList(){
-  return(
+function TasksList() {
+  return (
     <div className="bg-[#1E293B] p-2 md:p-4 rounded-sm md:rounded-lg">
       <div>
         <h1 className="text-xl text-gray-300">Your Tasks</h1>
       </div>
-      <div>
+      <div className="pt-2 flex justify-between">
+        <div>
+          <h1 className="text-gray-500 text-lg">1. Your first task</h1>
+        </div>
+        <div className="flex gap-x-2">
+          <button className="bg-red-400 text-md px-4 py-2  rounded-sm ">Edit</button>
+          <button className="bg-red-400 text-md px-4 py-2 rounded-sm ">Mark as Done</button> 
+          <button className="bg-red-400 text-md px-4 py-2 rounded-sm ">Delete</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 export default TasksList;
